4e26c91a662413da33f2a71e67e325ac1671952d
Ipar <pankaj.ipar@telus.com>
2026-01-31 09:50:51 +0530

    airbnb project add

---
+++ airbnb/models/src/src_listings.sql
@@ -0,0 +1,15 @@
+
+With raw_listing as 
+(Select * from AIRBNB.RAW.RAW_LISTINGS)
+
+Select 
+     id AS listing_id,
+    name AS listing_name,
+    listing_url,
+    room_type,
+    minimum_nights,
+    host_id,
+    price AS price_str,
+    created_at,
+    updated_at
+From raw_listing
